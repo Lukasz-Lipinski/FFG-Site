eebfc211aa44a0e7bcfec5ab7b3f1bd820720866
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2026-04-25 22:47:57 +0200

    footer added

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,57 @@
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { IIconLink } from '../../models/nav.models';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-footer',
+  imports: [NgOptimizedImage, MatIconModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Footer implements OnInit {
+  private readonly matIconRegistry = inject(MatIconRegistry);
+  private readonly sanitizer = inject(DomSanitizer);
+
+  private readonly logoPath = 'assets/images/logo.png';
+
+  readonly iconLinks: IIconLink[] = [
+    {
+      to: 'https://www.instagram.com',
+      label: 'Instagram',
+      icon: 'instagram',
+    },
+    {
+      to: 'https://www.facebook.com',
+      label: 'Facebook',
+      icon: 'facebook',
+    },
+    {
+      to: 'https://www.spotify.com',
+      label: 'Spotify',
+      icon: 'spotify',
+    },
+    {
+      to: 'https://www.youtube.com',
+      label: 'YouTube',
+      icon: 'youtube',
+    },
+  ];
+
+  get logo(): string {
+    return this.logoPath;
+  }
+
+  ngOnInit(): void {
+    const brandIcons = ['instagram', 'facebook', 'spotify', 'youtube'];
+    
+    brandIcons.forEach(icon => {
+      this.matIconRegistry.addSvgIcon(
+        icon,
+        this.sanitizer.bypassSecurityTrustResourceUrl(`assets/brands/${icon}.svg`)
+      );
+    });
+  }
+}
